ac29d8f5d058f7c72b0630e575808fe5ba72d56d
YiXingYuFeng <YiXingYuFeng@users.noreply.github.com>
2022-02-22 18:07:04 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,38 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1645517707324_8077';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // 页面配置
+  config.view = {
+    defaultViewEngine: 'ejs',
+    mapping: {
+      '.html': 'ejs',
+    },
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
